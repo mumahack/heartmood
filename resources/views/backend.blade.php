60239058dceb6ed0fd762f143af81161a430ae59
Elias Kotlyar <elias.kotlyar@gmail.com>
2017-10-08 12:37:40 +0200

    Debug

--- resources/views/backend.blade.php
+++ resources/views/backend.blade.php
@@ -99,3 +99,69 @@ Trigger : FSOC068255
         });
     });
 </script>
+
+<script src="mediaelement/mediaelement-and-player.min.js"></script>
+<link rel="stylesheet" href="mediaelement/mediaelementplayer.css"/>
+
+
+<script src="plugins/speed/speed.min.js"></script>
+<script src="plugins/speed/speed-i18n.js"></script>
+<link rel="stylesheet" href="plugins/speed/speed.min.css">
+
+<script>
+
+    var currentVideo = null;
+    var player = null;
+    function yourFunction() {
+        // do whatever you like here
+        $.getJSON('getvalue', function (data) {
+            newVideo = currentVideo;
+            switch (parseInt(data.heartbeat)) {
+                case 1:
+                    newVideo = "cLNjP1vkyYU";
+                    break;
+                case 2:
+                    newVideo = "1TsVjvEkc4s";
+                    break;
+                case 3:
+                    newVideo = "UXxRyNvTPr8";
+                    break;
+            }
+            $('#heartbeat').html(data.heartbeat);
+
+            if (newVideo != currentVideo) {
+                console.log("need to play following video"+currentVideo);
+                currentVideo = newVideo;
+                var myVideoPlayer = player;
+                console.log(myVideoPlayer);
+                myVideoPlayer.media.youTubeApi.loadVideoById(currentVideo);
+                myVideoPlayer.media.load();
+                myVideoPlayer.media.play();
+            }
+
+
+        });
+
+        setTimeout(yourFunction, 1000);
+    }
+
+
+    $(document).ready(function () {
+
+        player = new MediaElementPlayer('youtube1', {
+
+        });
+        yourFunction();
+
+
+    });
+
+</script>
+Heartbeat:
+<div id="heartbeat"></div>
+<div id=""></div>
+
+
+<video id="youtube1" width="640" height="360">
+    <source src="http://www.youtube.com/watch?v=nOEw9iiopwI" type="video/youtube">
+</video>
