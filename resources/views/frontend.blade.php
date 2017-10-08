c8ae30b49757a679c5da48062548eb34d73a0902
Elias Kotlyar <elias.kotlyar@gmail.com>
2017-10-08 10:51:46 +0200

    Frontend Working now

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -1,1 +1,26 @@
-Hello World!
+<script
+        src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+        crossorigin="anonymous"></script>
+
+<script>
+
+    function yourFunction(){
+        // do whatever you like here
+        $.getJSON('getvalue', function (data) {
+            $('#heartbeat').html( data.heartbeat);
+        });
+
+        setTimeout(yourFunction, 1000);
+    }
+
+
+
+
+    $(document).ready(function () {
+        yourFunction();
+    });
+</script>
+Heartbeat:
+<div id="heartbeat"></div>
+<div id=""></div>
